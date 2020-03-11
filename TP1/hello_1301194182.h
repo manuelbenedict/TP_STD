bc929c902de5c42ff2be872493b1250732a423d7
Manuel Benedict <59924448+manuelbenedict@users.noreply.github.com>
2020-03-12 00:08:25 +0700

    Add files via upload

---
+++ TP1/hello_1301194182.h
@@ -0,0 +1,15 @@
+#ifndef HELLO_1301194182_H_INCLUDED
+#define HELLO_1301194182_H_INCLUDED
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void greetings(string x, string y);
+void swap_1 (int a, int b, int c);
+void swap_2 (int a, int &b, int &c);
+void confused ();
+void remove_inside(int);
+string indexing (double, double, double);
+#endif // HELLO_1301194182_H_INCLUDED
